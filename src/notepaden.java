f4912a1c6e94dd8d735ec35429ebf6c8b137a032
Segerkung <60388987+Segerkung@users.noreply.github.com>
2021-02-23 11:17:34 +0100

    Förstår inte mig på action listener, och är lite lost sen jag va sjuk. Men ska försöka lära mig detta i vecka

--- src/notepaden.java
+++ src/notepaden.java
@@ -1,10 +1,17 @@
 import javax.swing.*;
+import java.awt.event.*;
+import java.awt.event.ActionListener;
+
+
 
 public class notepaden {
     public static void main(String[] args) {
         JFrame frame = new JFrame("notepaden");
+        JPanel panel = new JPanel();
+        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
         frame.setContentPane(new notepaden().RootPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
         frame.pack();
         frame.setVisible(true);
     }
@@ -12,3 +19,4 @@ public class notepaden {
     private JPanel RootPanel;
     private JTextArea textArea;
 }
+//För någon anledning kan jag inte implementa actionlistener, och förstår inte varför
